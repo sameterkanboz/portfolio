9f831affce650025d1906549f5da6f074cf28834
Samet Erkan Boz <samet.erkan.boz@gmail.com>
2025-05-03 18:46:03 +0300

    Refactor code structure for improved readability and maintainability

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -71,7 +71,7 @@ const Intro = () => {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/sameterkanboz_cv_en_2024.pdf"
+          href="/sameterkanboz.eng.pdf"
           download={true}
         >
           Özgeçmişimi İndir{" "}
